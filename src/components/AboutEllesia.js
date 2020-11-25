67e54e55addb7489da53048e06a7f2f14d7dae34
Ellesia Janto <jantoellesia@gmail.com>
2020-11-25 10:21:48 +0100

    pdf viewer

--- src/components/AboutEllesia.js
+++ src/components/AboutEllesia.js
@@ -6,10 +6,20 @@ import { TextField, Typography, Button, Grid, Box} from "@material-ui/core";
 
 const useStyles = makeStyles(theme=>({
     box:{
-        paddingTop: "150px"
+        paddingTop: "150px",
+        width: "80%"
     },
-    word:{
-        textTransform: "uppercase"
+    box2:{
+        paddingTop: "50px",
+        width: "80%",
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        alignContent: "center"
+
+    },
+    img: {
+        width: "300px"
     }
 
 }))
@@ -26,10 +36,18 @@ const classes = useStyles();
              <Box component="div" style={{background:"#C38D9E", height:"100vh"}}>
                 <Grid container justify="center">
                     <Box className={classes.box}>
-                        <Typography variant="h3" className={classes.word}>
-                            Coming Soon...
+                    <Typography variant="h2" >
+                        About Me
+                    </Typography>
+                        <Typography variant="h6" className={classes.word}>
+                            My name is Ellesia. I am an American Frontend Web Developer living in Germany.
+                            I shifted in to the world of web development at age 23, looking for a challenging and 
+                            exciting new career. The digital world won me over and I transitioned right into web dev. 
+                            I make responsive dynamic designs, that are both practical and fun!
+                            In my free time you will find me traveling throughout Europe or creating latte art designs.
                         </Typography>
                     </Box>
+                   
                 </Grid>
            </Box>
         </>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,70 +1,39 @@
 import React, { useState } from "react";
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from "react-pdf";
+import testPdf from "../doc/Ellesia_J_Janto.pdf";
 
-import pdf from '../bachelorUSA.pdf';
+// More examples here
+// https://github.com/wojtekmaj/react-pdf/wiki/Recipes
 
-function Resume() {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
-  
-    function onDocumentLoadSuccess({ numPages }) {
-      setNumPages(numPages);
-      setPageNumber(1);
-    }
-  
-    function changePage(offset) {
-      setPageNumber(prevPageNumber => prevPageNumber + offset);
-    }
-  
-    function previousPage() {
-      changePage(-1);
-    }
-  
-    function nextPage() {
-      changePage(1);
-    }
-  
- console.log(pdf)
-  
-    return (
-      <>
-        <Document
-          file={process.env.PUBLIC_URL + pdf}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
+// You could also pass the file as a URL to the Document component as such:
+const url =
+  "https://s3-ap-southeast-1.amazonaws.com/happay-local/HVP/BILL20198261213473719445688HP.pdf";
+
+const Resume = () => {
+  // PDFjs worker from an external cdn
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setPageNumber(1);
+  };
+  return (
+    <>
+      <div>
+        <Document file={testPdf} onLoadSuccess={onDocumentLoadSuccess}>
           <Page pageNumber={pageNumber} />
         </Document>
-        <div>
-          <p>
-            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-          </p>
-          <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-            Previous
-          </button>
-          <button
-            type="button"
-            disabled={pageNumber >= numPages}
-            onClick={nextPage}
-          >
-            Next
-          </button>
-        </div>
-      </>
-    );
-  }
-
-
-
-
-
-{/* <div style={{position: 'absolute', width: '100%', height: '100%'}}>
-                <object
-                data={require('../doc/EllesiaJantoEuropeanResume.pdf')}
-                type="application/pdf"
-                width="100%"
-                height="100%"
-                >
-               </object>
-            </div> */}
+       {/*  <p>
+          Page {pageNumber} of {numPages}
+        </p> */}
+       {/*  {Array.from(new Array(numPages), (el, index) => (
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      ))} */}
+      </div>
+    </>
+  );
+};
 
 export default Resume;
